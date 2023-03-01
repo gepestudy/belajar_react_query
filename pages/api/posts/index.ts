902f1a5dc8ddd8b1c85d5677a2738846f1b0b0f1
ilhamgepe <gepedevelop@gmail.com>
2023-03-01 12:59:28 +0700

    test commit ganti asc

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -14,7 +14,7 @@ export default async function handler(
     const data = await prisma.post.findMany({
       orderBy: [
         {
-          createdAt: "desc",
+          createdAt: "asc",
         },
       ],
     });
